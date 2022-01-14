9c87682c35e52943d445442dc7c5e7c2a5aeafc6
Nick <34692389+NickByers@users.noreply.github.com>
2022-01-14 10:41:58 -0500

    Add files via upload
    Version of Ellipse developed in lab

--- lab01/Ellipse.java
+++ lab01/Ellipse.java
@@ -1,25 +1,26 @@
-import java.awt.*;
+import java.awt.*; 
 import java.awt.geom.*;
-import javax.swing.JComponent;
+import javax.swing.JComponent; 
 import javax.swing.JFrame;
 
-public class Ellipse extends JComponent {
-    public void paint(Graphics g) {
-    Graphics2D g2 = (Graphics2D) g;
-    Ellipse2D ellipseBlack = new Ellipse2D.Double(25,25, getWidth()-50, getHeight()-50);
-    Ellipse2D ellipseBlue = new Ellipse2D.Double(50,50, getWidth()-100, getHeight()-100);
-    g2.setColor(Color.BLACK);
-    g2.fill(ellipseBlack);
-    g2.draw(ellipseBlack);
-    g2.setColor(Color.BLUE);
-    g2.fill(ellipseBlue);
-    g2.draw(ellipseBlue);
-}
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Ellipse");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Ellipse());
-        frame.setSize(new Dimension(500, 500));
-        frame.setVisible(true);
-    }
+
+public class Ellipse extends JComponent{
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		Shape ell = new Ellipse2D.Float(0, 0, getWidth() - 10, getHeight() - 10);
+
+		g2.setPaint(Color.BLUE); 
+		g2.fill(ell);
+		g2.setStroke(new BasicStroke(4));
+		g2.setPaint(Color.BLACK);
+		g2.draw(ell);
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Ellipse"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new Ellipse());
+		frame.setSize(new Dimension(500, 500));
+		frame.setVisible(true);
+	}
 }
